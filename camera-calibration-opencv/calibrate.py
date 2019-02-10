4c44788dac996417c6a86b262fe1436e8b5b201a
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-02-10 22:08:45 +0530

    Add Calibrate

--- camera-calibration-opencv/calibrate.py
+++ camera-calibration-opencv/calibrate.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import glob
 import matplotlib.pyplot as plt
+import pickle
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*8,3), np.float32)
@@ -35,3 +36,27 @@ for idx, fname in enumerate(images):
         cv2.waitKey(500)
 
 cv2.destroyAllWindows()
+
+# Test undistortion on an image
+img = cv2.imread('calibration_wide/test_image.jpg')
+img_size = (img.shape[1], img.shape[0])
+
+# Do camera calibration given object points and image points
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+
+
+dst = cv2.undistort(img, mtx, dist, None, mtx)
+cv2.imwrite('calibration_wide/test_undist.jpg',dst)
+
+# Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+dist_pickle = {}
+dist_pickle["mtx"] = mtx
+dist_pickle["dist"] = dist
+pickle.dump( dist_pickle, open( "calibration_wide/wide_dist_pickle.p", "wb" ) )
+#dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+# Visualize undistortion
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=30)
+ax2.imshow(dst)
+ax2.set_title('Undistorted Image', fontsize=30)
